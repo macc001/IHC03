ff2115d57030dd132f5f332a5515805585e08aed
macc001 <miguelcayocarvajal37@gmail.com>
2020-02-12 18:10:35 -0400

    add

--- src/ihc03/Interfaz01.java
+++ src/ihc03/Interfaz01.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -42,71 +43,75 @@ public class Interfaz01 extends JFrame {
     }
 
     public Interfaz01() {
-        ventana("Ventana principal");
-        etiqueta("componentes");
-        input("texto");
-        String elementos[] = {"Seleccione", "Elemento 1", "Elemento 2", "Elemento 3"};
-        combo(elementos);
-        boton("click", "Mensaje");
-        btnRadios("Radio Button1", 10, 151, 109, 23);
-        btnRadios("Radio Button2", 10, 177, 109, 23);
+        ventana("Formulario Interfaz01", 300, 200, 458, 363);
+        etiqueta("Introducir nombre", 10, 11, 500, 14);
+        input("Juan", 30, 36, 152, 20);
+        String elementos[] = {"Seleccio sexo", "Musciliton", "Femenino", "Otro"};
+        combo(elementos, 10, 67, 191, 20);
 
-        checkB("Check1", 10, 203, 166, 23);
-        checkB("Check2", 10, 229, 152, 23);
+        ButtonGroup bgroup = new ButtonGroup();
+        etiqueta("Elejir Departamento", 10, 100, 500, 14);
+        btnRadios("Santa Cruz", bgroup, 10, 120, 109, 23);
+        btnRadios("Tarija", bgroup, 10, 145, 109, 23);
 
-        textArea();
-        contra();
-        incrementador();
-        separador();
+        checkB("Recordar contrtaseña", 10, 170, 152, 23);
+
+        textArea("comentario", 224, 11, 205, 58);
+        contra("Contraseña", 224, 80, 152, 20);
+        boton("Guardar", "Guardado Exitosamente", 50, 200, 89, 23);
+
+        incrementador(224, 111, 101, 20);
+        separador(211, 11, 2, 297);
 
     }
 
-    private void ventana(String titulo) {
+    private void ventana(String titulo, int x, int y, int ancho, int alto) {
         this.setTitle(titulo);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBounds(300, 200, 458, 363);
+        this.setBounds(x, y, ancho, alto);
         contentPane = new JPanel();
         this.setContentPane(contentPane);
         contentPane.setLayout(null);
     }
 
-    private void etiqueta(String componentes) {
+    private void etiqueta(String componentes, int x, int y, int ancho, int alto) {
         JLabel lblEtiqueta = new JLabel(componentes);
-        lblEtiqueta.setBounds(10, 11, 500, 14);
+        lblEtiqueta.setBounds(x, y, ancho, alto);
         contentPane.add(lblEtiqueta);
     }
 
-    private void input(String texto) {
+    private void input(String texto, int x, int y, int ancho, int alto) {
         txtCajaDeTexto = new JTextField();
         txtCajaDeTexto.setText(texto);
-        txtCajaDeTexto.setBounds(30, 36, 152, 20);
+        txtCajaDeTexto.setBounds(x, y, ancho, alto);
         contentPane.add(txtCajaDeTexto);
         txtCajaDeTexto.setColumns(10);
     }
 
-    private void combo(String[] elementos) {
+    private void combo(String[] elementos, int x, int y, int ancho, int alto) {
         JComboBox comboBox = new JComboBox();
         comboBox.setModel(new DefaultComboBoxModel(elementos));
         comboBox.setSelectedIndex(0);
-        comboBox.setBounds(10, 67, 191, 20);
+        comboBox.setBounds(x, y, ancho, alto);
         contentPane.add(comboBox);
     }
 
-    private void boton(String click, String mensaje) {
+    private void boton(String click, String mensaje, int x, int y, int ancho, int alto) {
         JButton btnBotn = new JButton(click);
         btnBotn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
                 JOptionPane.showMessageDialog(frame, mensaje);
             }
         });
-        btnBotn.setBounds(53, 95, 89, 23);
+        btnBotn.setBounds(x, y, ancho, alto);
         contentPane.add(btnBotn);
     }
 
-    private void btnRadios(String texto, int x, int y, int ancho, int alto) {
+    private void btnRadios(String texto, ButtonGroup bgroup, int x, int y, int ancho, int alto) {
         JRadioButton rdbtn1 = new JRadioButton(texto);
         rdbtn1.setBounds(x, y, ancho, alto);
         contentPane.add(rdbtn1);
+        bgroup.add(rdbtn1);
     }
 
     private void checkB(String texto, int x, int y, int ancho, int alto) {
@@ -115,32 +120,32 @@ public class Interfaz01 extends JFrame {
         contentPane.add(chckbx1);
     }
 
-    private void textArea() {
+    private void textArea(String texto, int x, int y, int ancho, int alto) {
         JTextArea txtrAreaDeTexto = new JTextArea();
         txtrAreaDeTexto.setBorder(new LineBorder(new Color(0, 0, 0)));
         txtrAreaDeTexto.setRows(3);
-        txtrAreaDeTexto.setText("Arearnderntexto.");
-        txtrAreaDeTexto.setBounds(224, 11, 205, 58);
+        txtrAreaDeTexto.setText(texto);
+        txtrAreaDeTexto.setBounds(x, y, ancho, alto);
         contentPane.add(txtrAreaDeTexto);
     }
 
-    private void incrementador() {
-        JSpinner spinner = new JSpinner();
-        spinner.setBounds(224, 111, 101, 20);
-        contentPane.add(spinner);
-    }
-
-    private void contra() {
+    private void contra(String texto, int x, int y, int ancho, int alto) {
         passwordField = new JPasswordField();
-        passwordField.setBounds(224, 80, 152, 20);
-        passwordField.setText("Contraseña");
+        passwordField.setBounds(x, y, ancho, alto);
+        passwordField.setText(texto);
         contentPane.add(passwordField);
     }
 
-    private void separador() {
+    private void incrementador(int x, int y, int ancho, int alto) {
+        JSpinner spinner = new JSpinner();
+        spinner.setBounds(x, y, ancho, alto);
+        contentPane.add(spinner);
+    }
+
+    private void separador(int x, int y, int ancho, int alto) {
         JSeparator separator = new JSeparator();
         separator.setOrientation(SwingConstants.VERTICAL);
-        separator.setBounds(211, 11, 2, 297);
+        separator.setBounds(x, y, ancho, alto);
         contentPane.add(separator);
     }
 }
